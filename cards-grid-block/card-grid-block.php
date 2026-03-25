64a34466e6ad4a76c403e3206a49e846046509ec
Aleksandr Slipchenko <oslipchenko@itmonks.com>
2026-03-25 10:48:31 +0200

    fixed file card-grid-block

--- cards-grid-block/card-grid-block.php
+++ cards-grid-block/card-grid-block.php
@@ -9,12 +9,30 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
+function cards_grid_block_load_textdomain() {
+	load_plugin_textdomain(
+		'cards-grid-block',
+		false,
+		dirname(plugin_basename(__FILE__)) . '/languages'
+	);
+}
+add_action('plugins_loaded', 'cards_grid_block_load_textdomain');
+
 function cards_grid_block_editor_assets() {
+	$asset_path = plugin_dir_path(__FILE__) . 'build/index.asset.php';
+	$asset      = file_exists($asset_path) ? require $asset_path : array('dependencies' => array(), 'version' => null);
+	$script_ver = !empty($asset['version']) ? $asset['version'] : filemtime(plugin_dir_path(__FILE__) . 'build/index.js');
+
+	$script_deps = !empty($asset['dependencies']) ? $asset['dependencies'] : array();
+
+	$css_path = plugin_dir_path(__FILE__) . 'build/style-index.css';
+	$css_ver  = file_exists($css_path) ? filemtime($css_path) : $script_ver;
+
 	wp_enqueue_script(
 		'cards-grid-block-editor',
 		plugin_dir_url(__FILE__) . 'build/index.js',
-		array('wp-blocks', 'wp-element', 'wp-editor', 'wp-components', 'wp-i18n', 'wp-block-editor'),
-		filemtime(plugin_dir_path(__FILE__) . 'build/index.js'),
+		$script_deps,
+		$script_ver,
 		true
 	);
 
@@ -22,27 +40,35 @@ function cards_grid_block_editor_assets() {
 		'cards-grid-block-editor-style',
 		plugin_dir_url(__FILE__) . 'build/style-index.css',
 		array(),
-		filemtime(plugin_dir_path(__FILE__) . 'build/style-index.css')
+		$css_ver
 	);
 }
 add_action('enqueue_block_editor_assets', 'cards_grid_block_editor_assets');
 
 
 function cards_grid_block_front_assets() {
+	$css_path = plugin_dir_path(__FILE__) . 'build/style-index.css';
+	$css_ver  = file_exists($css_path) ? filemtime($css_path) : false;
+
+	$masonry_path = plugin_dir_path(__FILE__) . 'js/masonry.js';
+	$masonry_ver  = file_exists($masonry_path) ? filemtime($masonry_path) : false;
+
 	wp_enqueue_style(
 		'cards-grid-block-style',
 		plugin_dir_url(__FILE__) . 'build/style-index.css',
 		array(),
-		filemtime(plugin_dir_path(__FILE__) . 'build/style-index.css')
+		$css_ver
 	);
 
-	wp_enqueue_script(
-		'cards-grid-block-masonry',
-		plugin_dir_url(__FILE__) . 'js/masonry.js',
-		array(),
-		filemtime(plugin_dir_path(__FILE__) . 'js/masonry.js'),
-		true
-	);
+	if ($masonry_ver) {
+		wp_enqueue_script(
+			'cards-grid-block-masonry',
+			plugin_dir_url(__FILE__) . 'js/masonry.js',
+			array(),
+			$masonry_ver,
+			true
+		);
+	}
 }
 add_action('enqueue_block_assets', 'cards_grid_block_front_assets');
 
